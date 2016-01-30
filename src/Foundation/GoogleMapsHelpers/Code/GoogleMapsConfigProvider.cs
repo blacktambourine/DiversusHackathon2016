a04d2b93c9daf72eb840833ea89796c93ad33aa5
unknown <pstysiak@PC0002.diversus.com.au>
2016-01-31 03:50:08 +0800

    Foundation projects now build in Gulp

---
+++ src/Foundation/GoogleMapsHelpers/Code/GoogleMapsConfigProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Diversus.Hackathon2016.Foundation.GoogleMapsHelpers
+{
+    public class GoogleMapsConfigProvider
+    {
+    }
+}

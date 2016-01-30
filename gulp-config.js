3643da859b34f2b62e1b3150345dde509b11f4a4
unknown <pstysiak@PC0002.diversus.com.au>
2016-01-30 12:53:10 +0800

    initial commit

---
+++ gulp-config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var config = {
+        websiteRoot: "C:\\inetpub\\wwwroot\\Habitat\\Website",
+        sitecoreLibraries: "C:\\inetpub\\wwwroot\\Habitat\\Website\\bin",
+        solutionName: "Habitat",
+    buildConfiguration: "Debug"
+    }
+    return config;
+}

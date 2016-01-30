d827fa02613cd62950ce500569f6ac6c454c4f69
unknown <dbrett@COM-DEV-DBRETT.diversus.com.au>
2016-01-30 22:00:14 +0800

    More work on the Sitecore templates

--- src/Feature/OpenDataAgent/Code/Diversus.Hackathon2016.Foundation.OpenDataAgent/Models/DataSet.cs
+++ src/Feature/OpenDataAgent/Code/Diversus.Hackathon2016.Foundation.OpenDataAgent/Models/DataSet.cs
@@ -1,4 +1,5 @@
-﻿using Sitecore.Data.Items;
+﻿using Sitecore;
+using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Foundation.SitecoreExtensions.Extensions;
 using System;
@@ -10,20 +11,83 @@ namespace Diversus.Hackathon2016.Foundation.OpenDataAgent.Models
 {
     public class DataSet
     {
+        private Item _dataset;
         public DataSet(Item dataset)
         {
             Assert.IsTrue(dataset.IsDerived(Templates.DataSet.ID),
                 string.Format("'{0}' must be derrived from '{1}'", nameof(dataset), Templates.Repository.ID));
+            _dataset = dataset;
         }
         //-	OpenDataSourceType
-        public string AggregationProvider { get; set; }
+        /// <summary>
+        /// The type of the aggregation provider. 
+        /// Will return null if unspecified.
+        /// Will return null and log an exception if specified incorrectly.
+        /// </summary>
+        public Type AggregationProvider
+        {
+            get
+            {
+                var typeString = StringUtil.GetString(_dataset[Templates.DataSet.AggregationProvider], string.Empty);
+                if (string.IsNullOrEmpty(typeString))
+                {
+                    return null; // No type to work with.
+                }
+                try
+                {
+                    return Type.GetType(typeString);
+                }
+                catch (Exception ex)
+                {
+                    // Log the error and allow the site to keep running as if this had been empty the whole time.
+                    Log.Error(string.Format("Error reading type for dataset '{0}'", _dataset.ID), ex);
+                    return null;
+                }
+            }
+        }
         //-	Source(url or media item)
-        public string Parameters { get; set; }
+        public Dictionary<string, string> Parameters
+        {
+            get
+            {
+                var paramString = StringUtil.GetString(_dataset[Templates.DataSet.ProviderConfig], string.Empty);
+                if (string.IsNullOrEmpty(paramString))
+                {
+                    // Return an empty dictionary so that dependent code only needs to check for the existence of its' own parameters.
+                    return new Dictionary<string, string>();
+                }
+                else
+                {
+                    // Sanitize newline characters - I remember having carriage return problems with SC 8.0
+                    // Can't be bothered testing what SC 8.1 does.
+                    var paramLines = paramString.Replace('\r', '\n').Replace("\n\n", "\n").Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // Check for only things in valid format.
+                    var validPrameters = paramLines.Select(x => x.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries)).Where(x => x.Count() == 2);
+
+                    // Reformat as a dictionary.
+                    return validPrameters.ToDictionary(x => x[0], x => x[1]);
+                }
+            }
+        }
+
         //-	Facet Name
-        public string Name { get; set; }
+        public string Name
+        {
+            get
+            {
+                return StringUtil.GetString(_dataset[Templates.DataSet.FacetName], string.Empty);
+            }
+        }
         //-	DataPointType
         public string Coordinates { get; set; }
         //-	Colour
-        public string Color { get; set; }
+        public string Colour
+        {
+            get
+            {
+                return StringUtil.GetString(_dataset[Templates.DataSet.FacetName], string.Empty);
+            }
+        }
     }
 }
--- src/Feature/OpenDataAgent/Code/Diversus.Hackathon2016.Foundation.OpenDataAgent/Repositories/OpenDataAgentRepository.cs
+++ src/Feature/OpenDataAgent/Code/Diversus.Hackathon2016.Foundation.OpenDataAgent/Repositories/OpenDataAgentRepository.cs
@@ -4,18 +4,31 @@ using System.Linq;
 using System.Web;
 using Sitecore.Data;
 using Diversus.Hackathon2016.Foundation.OpenDataAgent.Models;
+using Sitecore.Diagnostics;
+using Sitecore.Foundation.SitecoreExtensions.Extensions;
 
 namespace Diversus.Hackathon2016.Foundation.OpenDataAgent.Repositories
 {
     public class OpenDataAgentRepository
-    {        
-        public OpenDataAgentRepository(ID repositoryID, Sitecore.Context context)
+    {
+        private Repository _internalRepository;
+        public OpenDataAgentRepository(ID repositoryID, Database database)
         {
-
+            Assert.IsNotNull(database, "Sitecore context cannot be null");
+            _internalRepository = new Repository(repositoryID, database);
         }
-        public OpenDataAgentRepository(Repository repository, Sitecore.Context context)
+        public OpenDataAgentRepository(Repository repository)
         {
-
+            Assert.IsNotNull(repository, "OpenData Repository cannot be null");
+            _internalRepository = repository;
+        }
+        public IEnumerable<DataSet> GetDataSets()
+        {
+            return _internalRepository.Datasets;
+        }
+        public static IEnumerable<OpenDataAgentRepository> GetRepositoriesUnderPath(string path, Database database)
+        {
+            return database.SelectItems(path).Where(x => x.IsDerived(Templates.Repository.ID)).Select(x=> new OpenDataAgentRepository(new Repository(x)));
         }
     }
 }
